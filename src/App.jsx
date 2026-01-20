734eaed56d98a87dbdc49b74054ef1c4afa3c989
yağmur gamze çimen <yagmurgamzecimen@gmail.com>
2026-01-20 11:34:05 +0300

    T01: initial project setup (Vite + React + Tailwind)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Tailwind OK</h1>
+    </>pwd
+
+  );
+}
+
+export default App;
